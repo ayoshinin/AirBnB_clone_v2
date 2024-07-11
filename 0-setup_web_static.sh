88bf41ac274f69dad58445559bac6176e304d68b
ayoshinin <ayodejisamson73@yahoo.com>
2024-07-11 09:53:34 -0700

    static file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,35 @@
 #!/usr/bin/env bash
-# Script that configures Nginx server with some folders and files
-sudo apt-get -y update
-sudo apt-get -y upgrade
+# Prepares the system for web application deployment
+sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-conf="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-sudo sed -i "45i $conf" /etc/nginx/sites-available/default
-sudo service nginx start
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+function has_location_block(){
+    grep -q 'location /hbnb_static {' /etc/nginx/sites-enabled/default
+}
+
+if has_location_block;then
+    echo "Location /hbnb_static already exists in the configuration file."
+else
+    sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+fi
+
 sudo service nginx restart
-sudo service nginx reload
